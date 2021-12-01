991de3ec5262b843bd1fae391a7b05514518efbe
luoqiang <516161832@qq.com>
2021-12-01 17:00:45 +0800

    init test

---
+++ framewrok-util/src/main/java/io/github/l86400/util/StringUtil.java
@@ -0,0 +1,34 @@
+package io.github.l86400.util;
+
+/**
+ * @author 86400
+ * @date 2021/12/1 16:24
+ */
+public class StringUtil {
+    private StringUtil() {
+
+    }
+
+    /**
+     * <p>Checks if a CharSequence is empty ("") or null.</p>
+     *
+     * <pre>
+     * StringUtils.isEmpty(null)      = true
+     * StringUtils.isEmpty("")        = true
+     * StringUtils.isEmpty(" ")       = false
+     * StringUtils.isEmpty("bob")     = false
+     * StringUtils.isEmpty("  bob  ") = false
+     * </pre>
+     *
+     * <p>NOTE: This method changed in Lang version 2.0.
+     * It no longer trims the CharSequence.
+     * That functionality is available in isBlank().</p>
+     *
+     * @param cs the CharSequence to check, may be null
+     * @return {@code true} if the CharSequence is empty or null
+     * @since 3.0 Changed signature from isEmpty(String) to isEmpty(CharSequence)
+     */
+    public static boolean isEmpty(final CharSequence cs) {
+        return cs == null || cs.length() == 0;
+    }
+}
---
+++ framewrok-util/src/test/java/io/github/l86400/util/StringUtilTest.java
@@ -0,0 +1,15 @@
+package io.github.l86400.util;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author  86400
+ * @date  2021/12/1 16:25
+ */
+class StringUtilTest {
+
+    @Test
+    void isEmpty() {
+
+    }
+}
